06a3d26c0ef12ac8083d89943a57b8ea7e9a8cfb
root <idandrei94@gmail.com>
2017-11-23 02:41:04 -0800

    added the python and chessnut lib

---
+++ chess.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3.5
+from random import randint
+from Chessnut import Game
+import subprocess
+import cgitb
+import cgi
+cgitb.enable()
+
+print("Content-Type: text/html")
+print()
+board = Game()
+moves = ""
+arg_moves = []
+try:
+ arg = cgi.FieldStorage()['history']
+ arg_moves = arg.value.split(' ')
+except:
+ pass
+for move in arg_moves:
+ try:
+  board.apply_move(move.strip())
+  moves = moves + " " + move
+ except:
+  pass
+available_moves = board.get_moves()
+chosen_move = available_moves[randint(0, len(available_moves)-1)]
+moves = moves + " " + chosen_move
+print(chosen_move)
